ffeaa4f7fd81b7d5a28c65953593a03357a8f8b9
Aung Kyaw Thant <akthant@gmail.com>
2020-03-10 17:36:42 +0800

    magic ball
    First commit Magic ball flutter exercise

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[500],
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int BallNo = 1;
+
+  void changeBallNo() {
+    setState(() {
+      BallNo = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeBallNo();
+              },
+              child: Image.asset('images/ball$BallNo.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
